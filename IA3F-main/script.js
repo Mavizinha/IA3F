6c51210bdc9b9d10463a4cc568dfa729297b8e36
Mavizinha <94861672+Mavizinha@users.noreply.github.com>
2024-09-18 18:24:06 -0300

    Add files via upload

---
+++ IA3F-main/script.js
@@ -0,0 +1,38 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
+
+const perguntas = [ //serve para abrir lista de perguntas
+    {//abre o objeto das perguntas
+        enunciado: "O que é a inteligencia artficial?",
+        alternativas: ["Uma pessoa considerada não inteligente","Algo que se aproxima da inteligencia humana, porém artificial"]
+    },
+    {
+        enunciado: "As IAs devem ser programadas para:",
+        alternativas: ["Tomar decisões autônomas","Seguir sempre as diretrizes humanas"]
+    },
+    {
+        enunciado: "A IA deve ser usada principalmente para",
+        alternativas: ["Aumentar a eficiência humana","Substituir completamente o trabalho humano"]
+    }
+]
+
+let posicao = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[posicao];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativa){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+
+    }
+    
+}
+mostraPergunta();
